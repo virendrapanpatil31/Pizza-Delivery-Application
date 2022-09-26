88428748707e141203cd4603cfaef0c66f74169f
Virendra Panpatil <VPanpatil@medisolv.com>
2022-09-26 19:47:32 +0530

    Mobile Support & View Components

---
+++ PizzaHub/ViewComponents/PizzaMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PizzaHub.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaHub.ViewComponents
+{
+    public class PizzaMenuViewComponent: ViewComponent
+    {
+        ICatalogService _catalogService;
+        public PizzaMenuViewComponent(ICatalogService catalogService)
+        {
+            _catalogService = catalogService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _catalogService.GetItems();
+            return View("~/Views/Shared/_PizzaMenu.cshtml",items);
+        }
+    }
+}
